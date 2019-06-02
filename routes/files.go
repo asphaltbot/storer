7227b354ae0fff4c19fe7b67a3dcb5a24f5ce632
Connor Wright <connor@asphaltbot.com>
2019-06-02 11:19:12 +0100

    ability to make the storage server download files

--- routes/files.go
+++ routes/files.go
@@ -1,17 +1,97 @@
 package routes
 
 import (
+	"encoding/json"
 	"github.com/asphaltbot/file-storage/util"
 	"github.com/gin-gonic/gin"
+	"io"
+	"io/ioutil"
+	"net/http"
 	"os"
 	"path"
 	"path/filepath"
+	"time"
 )
+
+type URLDownload struct {
+	URL string `json:"url"`
+}
+
+var httpClient = &http.Client{Timeout: 10 * time.Second}
 
 func RegisterUploadRoutes(e *gin.Engine) {
 	e.GET("/file/:id", FetchFileByID)
 	e.POST("/upload", UploadFile)
+	e.POST("/download", DownloadFile)
 	e.DELETE("/file/:id", DeleteFileByID)
+}
+
+func DownloadFile(c *gin.Context) {
+	bodyBytes, err := ioutil.ReadAll(c.Request.Body)
+
+	if err != nil {
+		c.AbortWithStatusJSON(200, gin.H{"code": 500, "message": "Unable to get request body"})
+		return
+	}
+
+	var urlDownload URLDownload
+	err = json.Unmarshal(bodyBytes, &urlDownload)
+
+	if err != nil {
+		c.AbortWithStatusJSON(200, gin.H{"code": 500, "message": err.Error()})
+		return
+	}
+
+	var dir string
+
+	if util.IsRunningInProd() {
+		dir = "/home/storage/user/"
+	} else {
+		dir = "D:\\"
+	}
+
+	resp, err := httpClient.Get(urlDownload.URL)
+
+	if err != nil {
+		c.AbortWithStatusJSON(200, gin.H{"code": 500, "message": err.Error()})
+		return
+	}
+
+	defer resp.Body.Close()
+	id := util.RandomString(8)
+
+	contentType := resp.Header.Get("content-type")
+
+	if contentType == "" {
+		c.AbortWithStatusJSON(200, gin.H{"code": 400, "message": "Content type could not be found for " + urlDownload.URL})
+		return
+	}
+
+	extension := filepath.Ext(urlDownload.URL)
+
+	if extension == "" {
+		c.AbortWithStatusJSON(200, gin.H{"code": 500, "message": "Unable to get extension for: " + urlDownload.URL})
+		return
+	}
+
+	out, err := os.Create(dir + id + extension)
+
+	if err != nil {
+		c.AbortWithStatusJSON(200, gin.H{"code": 500, "message": err.Error()})
+		return
+	}
+
+	defer out.Close()
+
+	_, err = io.Copy(out, resp.Body)
+
+	if err != nil {
+		c.AbortWithStatusJSON(200, gin.H{"code": 500, "message": err.Error()})
+		return
+	}
+
+	c.JSON(200, gin.H{"code": 200, "message": "Successfully downloaded", "id": id})
+
 }
 
 func UploadFile(c *gin.Context) {
