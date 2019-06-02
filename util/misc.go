d4149335ef36079fd7d19272e78c83a12141dc47
Connor Wright <connor@asphaltbot.com>
2019-06-02 11:18:39 +0100

    seed the rng

--- util/misc.go
+++ util/misc.go
@@ -1,10 +1,17 @@
 package util
 
-import "math/rand"
+import (
+	"math/rand"
+	"time"
+)
 
 var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
 
 func RandomString(length int) string {
+
+	now := time.Now()
+	rand.Seed(now.UnixNano())
+
 	b := make([]rune, length)
 	for i := range b {
 		b[i] = letterRunes[rand.Intn(len(letterRunes))]
