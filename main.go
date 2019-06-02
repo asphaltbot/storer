7b11e4be8f1c810dcdddbb78c5ed0ae1249af2a4
Connor Wright <connor@asphaltbot.com>
2019-06-02 11:19:46 +0100

    update / route

--- main.go
+++ main.go
@@ -21,12 +21,12 @@ func main() {
 	r.Use(gzip.Gzip(gzip.DefaultCompression))
 
 	r.Use(cors.New(cors.Config{
-		AllowAllOrigins: true,
-		AllowMethods: []string{"GET", "POST", "DELETE"},
-		AllowHeaders: []string{"Origin"},
-		ExposeHeaders: []string{"Content-Length"},
+		AllowAllOrigins:  true,
+		AllowMethods:     []string{"GET", "POST", "DELETE"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
-		MaxAge: 24 * time.Hour,
+		MaxAge:           24 * time.Hour,
 	}))
 
 	registerRoutes(r)
@@ -37,9 +37,9 @@ func main() {
 		gin.SetMode(gin.ReleaseMode)
 
 		m := autocert.Manager{
-			Prompt: autocert.AcceptTOS,
+			Prompt:     autocert.AcceptTOS,
 			HostPolicy: autocert.HostWhitelist("storage.asphaltbot.com"),
-			Cache: autocert.DirCache("/home/storage/cache"),
+			Cache:      autocert.DirCache("/home/storage/cache"),
 		}
 
 		log.Fatal(autotls.RunWithManager(r, &m))
@@ -52,9 +52,8 @@ func main() {
 }
 
 func Index(c *gin.Context) {
-	c.Data(200, "text/html", []byte("<!DOCTYPE html><html lang=\"en\" dir=\"ltr\"> <head> <meta charset=\"utf-8\"> <title>Asphalt Storage Server</title> <link href=\"https://fonts.googleapis.com/css?family=Roboto&display=swap\" rel=\"stylesheet\"> <style>h1, p, small{font-family: 'Roboto', sans-serif;}</style> </head> <body> <div align=\"center\"> <h1>Asphalt Storage Server</h1> <p>This server is running custom open source backup software, click <a href=\"https://github.com/asphaltbot/file-storage\">here</a> for more information.</p><br/> <small>Created by Connor Wright</small> </div></body></html>"))
+	c.Data(200, "text/html", []byte("<!DOCTYPE html><html lang=\"en\" dir=\"ltr\"> <head> <meta charset=\"utf-8\"> <title>Asphalt Storage Server</title> <link href=\"https://fonts.googleapis.com/css?family=Roboto&display=swap\" rel=\"stylesheet\"> <style>h1, p, small{font-family: 'Roboto', sans-serif;}</style> </head> <body> <div align=\"center\"> <h1>Asphalt Storage Server</h1> <p>This server is running custom open source storage software, click <a href=\"https://github.com/asphaltbot/file-storage\">here</a> for more information.</p><br/> <small>Created by Connor Wright</small> </div></body></html>"))
 }
-
 
 func registerRoutes(e *gin.Engine) {
 	routes.RegisterUploadRoutes(e)
